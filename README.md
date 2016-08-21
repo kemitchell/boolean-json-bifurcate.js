14e03acd8f1bfb79e1b1cab5f115477257be25b2
Kyle E. Mitchell <kyle@kemitchell.com>
2016-08-21 15:10:48 -0700

    Use JavaScript Standard Style

--- README.md
+++ README.md
@@ -3,18 +3,22 @@ var assert = require('assert')
 var bifurcate = require('boolean-json-bifurcate')
 
 assert.deepEqual(
-  bifurcate({ and: [ 'p', 'q' ] }),
-  { and: [ 'p', 'q' ] })
+  bifurcate({and: ['p', 'q']}),
+  {and: ['p', 'q']}
+)
 
 assert.deepEqual(
-  bifurcate({ and: [ 'p', 'q', 'r', 's' ] }),
-  { and: [ 'p', { and: [ 'q', { and: [ 'r', 's' ] } ] } ] })
+  bifurcate({and: ['p', 'q', 'r', 's']}),
+  {and: ['p', {and: ['q', {and: ['r', 's']}]}]}
+)
 
 assert.deepEqual(
-  bifurcate({ or: [ 'p', 'q', 'r', 's' ] }),
-  { or: [ 'p', { or: [ 'q', { or: [ 'r', 's' ] } ] } ] })
+  bifurcate({or: ['p', 'q', 'r', 's']}),
+  {or: ['p', {or: ['q', {or: ['r', 's']}]}]}
+)
 
 assert.deepEqual(
-  bifurcate({ not: { or: [ { not: 'p' }, 'q', 'r' ] } }),
-  { not: { or: [ { not: 'p' }, { or: [ 'q', 'r' ] } ] } })
+  bifurcate({not: {or: [{not: 'p'}, 'q', 'r']}}),
+  {not: {or: [{not: 'p'}, {or: ['q', 'r']}]}}
+)
 ```
--- index.js
+++ index.js
@@ -1,38 +1,44 @@
-module.exports = bifurcate
-
-function bifurcate(argument) {
+module.exports = function bifurcate (argument) {
   if (negation(argument)) {
-    return { not: bifurcate(argument.not) } }
-  else if (conjunction(argument)) {
+    return {not: bifurcate(argument.not)}
+  } else if (conjunction(argument)) {
     return argument.and
-      .reverse()
-      .reduce(function(last, conjunct) {
-        return (
-          last ?
-            { and: [ bifurcate(conjunct), last ] } :
-            bifurcate(conjunct) ) }) }
-  else if (disjunction(argument)) {
+    .reverse()
+    .reduce(function (last, conjunct) {
+      return last
+      ? {and: [bifurcate(conjunct), last]}
+      : bifurcate(conjunct)
+    })
+  } else if (disjunction(argument)) {
     return argument.or
-      .reverse()
-      .reduce(function(last, disjunct) {
-        return (
-          last ?
-            { or: [ bifurcate(disjunct), last ] } :
-            bifurcate(disjunct) ) }) }
-  else {
-    return argument } }
+    .reverse()
+    .reduce(function (last, disjunct) {
+      return last
+      ? {or: [bifurcate(disjunct), last]}
+      : bifurcate(disjunct)
+    })
+  } else {
+    return argument
+  }
+}
 
-function conjunction(argument) {
+function conjunction (argument) {
   return (
     typeof argument === 'object' &&
-    'and' in argument ) }
+    'and' in argument
+  )
+}
 
-function disjunction(argument) {
+function disjunction (argument) {
   return (
     typeof argument === 'object' &&
-    'or' in argument ) }
+    'or' in argument
+  )
+}
 
-function negation(argument) {
+function negation (argument) {
   return (
     typeof argument === 'object' &&
-    'not' in argument ) }
+    'not' in argument
+  )
+}
