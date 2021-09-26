97e470a4d0ab933725b87df9cdc08a9f430087f7
Kyle E. Mitchell <kyle@kemitchell.com>
2021-09-25 20:44:48 -0700

    Improve code style

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
 module.exports = function bifurcate (argument) {
-  if (negation(argument)) {
+  if (isNegation(argument)) {
     return {not: bifurcate(argument.not)}
-  } else if (conjunction(argument)) {
+  } else if (isConjunction(argument)) {
     return argument.and
-    .reverse()
-    .reduce(function (last, conjunct) {
-      return last
-      ? {and: [bifurcate(conjunct), last]}
-      : bifurcate(conjunct)
-    })
-  } else if (disjunction(argument)) {
+      .reverse()
+      .reduce(function (last, conjunct) {
+        return last
+          ? {and: [bifurcate(conjunct), last]}
+          : bifurcate(conjunct)
+      })
+  } else if (isDisjunction(argument)) {
     return argument.or
-    .reverse()
-    .reduce(function (last, disjunct) {
-      return last
-      ? {or: [bifurcate(disjunct), last]}
-      : bifurcate(disjunct)
-    })
+      .reverse()
+      .reduce(function (last, disjunct) {
+        return last
+          ? {or: [bifurcate(disjunct), last]}
+          : bifurcate(disjunct)
+      })
   } else {
     return argument
   }
 }
 
-function conjunction (argument) {
+function isNegation (argument) {
+  return (
+    typeof argument === 'object' &&
+    'not' in argument
+  )
+}
+
+function isConjunction (argument) {
   return (
     typeof argument === 'object' &&
     'and' in argument
   )
 }
 
-function disjunction (argument) {
+function isDisjunction (argument) {
   return (
     typeof argument === 'object' &&
     'or' in argument
-  )
-}
-
-function negation (argument) {
-  return (
-    typeof argument === 'object' &&
-    'not' in argument
   )
 }
